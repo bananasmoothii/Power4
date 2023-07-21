3af3ec96570e77e1d544143e50351f45c3fbf5c9
Bananasmoothii <45853225+bananasmoothii@users.noreply.github.com>
2023-07-21 22:25:34 +0200

    Initial commit

---
+++ src/game/Power4Board.cpp
@@ -0,0 +1,5 @@
+//
+// Created by bananasmoothii on 21/07/2023.
+//
+
+#include "Power4Board.hpp"
---
+++ src/game/Power4Board.hpp
@@ -0,0 +1,14 @@
+//
+// Created by bananasmoothii on 21/07/2023.
+//
+
+#ifndef POWER4_POWER4BOARD_HPP
+#define POWER4_POWER4BOARD_HPP
+
+
+class Power4Board {
+
+};
+
+
+#endif //POWER4_POWER4BOARD_HPP
